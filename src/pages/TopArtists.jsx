3e35c3feaa149ea4c9d04525a9333e87504db9c0
MertArar <85193329+MertArar@users.noreply.github.com>
2023-05-23 01:46:40 +0300

    topArtists added

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,22 @@
-const TopArtists = () => <div>TopArtists</div>;
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { Error, Loader, ArtistCard } from "../components";
 
+const TopArtists = () => {
+  const { data, isFetching, error } = useGetTopChartsQuery();
+
+  if (isFetching) return <Loader title="Loading songs top charts" />;
+
+  if (error) return <Error />;
+
+  return (
+    <div>
+      <h2>Top Artists</h2>
+      <div>
+        {data?.map((track) => (
+          <ArtistCard key={track.key} track={track} />
+        ))}
+      </div>
+    </div>
+  );
+};
 export default TopArtists;
