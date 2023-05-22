2397485962b75ba3fb768110a44223247299340e
MertArar <85193329+MertArar@users.noreply.github.com>
2023-05-22 05:09:35 +0300

    songCard added

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,51 @@
-const Discover = () => <div>Discover</div>;
+import { useDispatch, useSelector } from "react-redux";
+
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+import { selectGenreListId } from "../redux/features/playerSlice";
+import { useGetSongsByGenreQuery } from "../redux/services/shazamCore";
+
+const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying, genreListId } = useSelector(
+    (state) => state.player
+  );
+  const { data, isFetching, error } = useGetSongsByGenreQuery(
+    genreListId || "POP"
+  );
+
+  if (isFetching) return <Loader title="Loader songs..." />;
+
+  const genreTitle = genres.find(({ value }) => value === genreListId)?.title;
+  return (
+    <div>
+      <div>
+        <h2>Discover {genreTitle}</h2>
+        <select
+          onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+          value={genreListId || "pop"}
+        >
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            i={i}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
